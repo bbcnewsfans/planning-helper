17730c58f26e9edc9da6e5bce5102e1520a3c671
valentine <v4lbuild3r@gmail.com>
2024-02-29 01:28:00 -0600

    slash command syncing

--- src/main.py
+++ src/main.py
@@ -12,4 +12,18 @@ async def on_ready():
     print(f'Logged in as {bot.user.display_name}')
     return
 
+@commands.command(name = "sync-app-commands",
+                  description = "Syncs application commands.")
+@commands.guild_only()
+@commands.cooldown(1, 2, commands.BucketType.member)
+async def syncAppCommands(ctx:commands.Context):
+    if ctx.author.id == 1191850547138007132:
+        async with ctx.typing():
+            await bot.tree.sync()
+        await ctx.reply(content="Commands synced!", mention_author=False)
+        return
+    else:
+        await ctx.reply(content="You don't have the permissions to run this command.", mention_author=False)
+        return
+
 bot.run(token)
