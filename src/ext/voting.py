03e314876325e2ef03625111084058b574bea2f3
Valentine <v4lbuild3r@gmail.com>
2024-02-29 14:55:36 -0600

    poll listener

--- src/ext/voting.py
+++ src/ext/voting.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from models.Poll import Poll
 
 
 class VotingCog(commands.Cog):
@@ -9,7 +10,21 @@ class VotingCog(commands.Cog):
 
 
     # listen for new button presses on polls
+    @commands.Cog.listener()
+    async def on_interaction(self, interaction: discord.Interaction):
+        if interaction.type == discord.InteractionType.component and interaction.data['custom_id'].startswith('poll-'):
+            pollData = interaction.data['custom_id'].split('-')
 
+            try:
+                Poll.addVote(pollData[1], pollData[2], interaction.user.id)
+            except TypeError:
+                await interaction.response.send_message(content="I ran into an error counting your vote.", ephemeral=True)
+                return
+
+            await interaction.response.send_message(content="I've counted your vote!", ephemeral=True)
+            return
+        else:
+            return
 
 def setup(bot:commands.Bot):
     bot.add_cog(VotingCog(bot))
