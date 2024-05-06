dd78bfc5e524a80bb51614a47aea5ce7c4b39104
valentine <v4lbuild3r@gmail.com>
2024-05-06 16:26:37 -0500

    new announcement relay system

--- src/config.ex.py
+++ src/config.ex.py
@@ -4,5 +4,10 @@ class NewsFansHelper:
     class AnnouncementRelay:
         from_server_id = 0
         from_channel_id = 0
+        ping_role_id = 0
+
+        class FlagRoleIds:
+            noping = 0
+            asme = 0
 
         to_webhook_url = ""
--- src/main.py
+++ src/main.py
@@ -15,23 +15,55 @@ async def on_ready():
 async def ping(ctx: commands.Context):
     await ctx.send(content=f"My ping is {round(bot.latency * 1000)}ms!")
 
-async def publish_to_announcement_channel(message: discord.Message):
-    if message.guild.id == config.NewsFansHelper.AnnouncementRelay.from_server_id and message.channel.id == config.NewsFansHelper.AnnouncementRelay.from_channel_id: # log
-        send_to = discord.Webhook.from_url(url=config.NewsFansHelper.AnnouncementRelay.to_webhook_url, client=bot)
+@bot.command()
+async def publish(ctx: commands.Context):
+    if bot.get_guild(config.NewsFansHelper.server_id).get_role(config.NewsFansHelper.board_role_id):
+        if ctx.channel.type in [discord.ChannelType.news_thread, discord.ChannelType.public_thread, discord.ChannelType.private_thread] and ctx.channel.parent.id == config.NewsFansHelper.AnnouncementRelay.from_channel_id:
+            send_to = discord.Webhook.from_url(url=config.NewsFansHelper.AnnouncementRelay.to_webhook_url, client=bot)
 
-        files = []
-        for attachment in message.attachments:
-            f = await attachment.to_file()
-            files.append(f)
+            msg = ctx.channel.starter_message
 
-        await send_to.send(content=message.content + f"\n\n<@&{config.NewsFansHelper.AnnouncementRelay.ping_role_id}>", files=files)
+            ping = f"\n\n<@&{config.NewsFansHelper.AnnouncementRelay.ping_role_id}>"
+            as_author = False
+
+            if msg:
+                content = msg.content
+
+                if f"<@&{config.NewsFansHelper.AnnouncementRelay.FlagRoleIds.noping}>" in content:
+                    content = content.replace(f"<@&{config.NewsFansHelper.AnnouncementRelay.FlagRoleIds.noping}>", "")
+                    ping = ""
+                
+                if f"<@&{config.NewsFansHelper.AnnouncementRelay.FlagRoleIds.asme}>" in content:
+                    content = content.replace(f"<@&{config.NewsFansHelper.AnnouncementRelay.FlagRoleIds.asme}>", "")
+                    as_author = True
+                
+                files = []
+
+                for attachment in msg.attachments:
+                    a = await attachment.to_file()
+                    files.append(a)
+                
+                if as_author:
+                    await send_to.send(content=(content + ping), files=files, username=msg.author.global_name, avatar_url=msg.author.display_avatar.url)
+                else:
+                    await send_to.send(content=(content + ping), files=files)
+                
+                await ctx.send(content="Published!")
+            else:
+                await ctx.send(content="Message not found!")
+                return
+        else:
+            await ctx.send(content="This is not a thread linked to an announcement!")
+            return
+    else:
+        await ctx.send(content="You don't have the permissions to run this command.")
+        return
 
 @bot.event
 async def on_message(message: discord.Message):
     text_types = [discord.ChannelType.text, discord.ChannelType.news]
     if message.channel.type in text_types:
         await autothread_from_message(message, planninghelperlog)
-        await publish_to_announcement_channel(message)
     await bot.process_commands(message)
 
 bot.run(config.NewsFansHelper.token)
